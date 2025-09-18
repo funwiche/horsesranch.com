f75f4e76d9a4ec2f3c02c05b4b2a730d4966f1ed
funwiche <funwiche.dev@gmail.com>
2025-09-18 20:03:50 +0100

    Update index.vue

--- pages/horses/index.vue
+++ pages/horses/index.vue
@@ -6,8 +6,24 @@
         Showing {{ starts + 1 }}-{{ starts + items.length }} of
         {{ products.length }} results
       </section>
-      <div class="lg grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <v-card :item="item" v-for="item in items" :key="item.id" />
+      <div
+        class="lg grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border border-t-0"
+      >
+        <div
+          class="p-4 border-t"
+          :class="
+            i % 4 != 3 && width >= 1280
+              ? 'border-r'
+              : i % 3 != 2 && width < 1280 && width >= 968
+              ? 'border-r'
+              : i % 2 != 1 && width < 968 && width >= 600
+              ? 'border-r'
+              : 'border-r-0'
+          "
+          v-for="(item, i) in items"
+        >
+          <v-card :item="item" class="h-full" />
+        </div>
       </div>
       <div class="py-6 flex gap-2 overflow-x-auto">
         <button
@@ -31,6 +47,7 @@
 import products from "@/resources/products.json";
 const route = useRoute();
 const limit = 24;
+const width = ref(0);
 const page = computed(() => parseInt((route.query.page as string) || "1"));
 const starts = computed(() => (page.value - 1) * limit);
 const total = computed(() => Math.ceil(products.length / limit));
@@ -42,4 +59,8 @@ function navigate(page: number) {
   navigateTo({ name: "horses", query: { ...route.query, page } });
   document.querySelector("#top")?.scrollIntoView();
 }
+onMounted(() => {
+  width.value = innerWidth;
+  window.addEventListener("resize", () => (width.value = innerWidth));
+});
 </script>
