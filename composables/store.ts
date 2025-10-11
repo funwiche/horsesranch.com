83a94552a38cfc7dd95c32e0461a9b146cfbd6d4
funwiche <funwiche.dev@gmail.com>
2025-10-11 22:18:58 +0100

    ok

--- composables/store.ts
+++ composables/store.ts
@@ -1,7 +1,7 @@
 import products from "@/resources/products.json";
 export const useFeatured = () =>
   useState("featured", () =>
-    [...products]
+    [...products.filter((el) => el.category == "Horses")]
       .slice()
       .sort(() => Math.random() - 0.5)
       .slice(0, 12)
