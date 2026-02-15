4fa5e57775b500c9dee6af36455d2cc4ced15ebf
funwiche <funwiche.dev@gmail.com>
2026-02-15 21:35:02 +0000

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -8,7 +8,7 @@ export const $app = {
   desc: "Find Your Perfect Equine Partner at Versatile Horses Ranch. We breed and raise quality family horses, confident trail companions, and versatile all-around mounts. Experience our gentle, well-started horses and find a trusted friend for riding, competition, or ranch work. Visit us to meet your new horse.",
   domain: "www.horsesranch.com",
   email: "info@horsesranch.com",
-  phone: "+1 (956) 502-0547",
+  phone: "+1 (956) 215-0140",
   address: "McAllen, Texas 78504, USA",
   hours: "Mon-Sat: 9:00 AM - 18:00 PM",
   api: "https://xtremailer.vercel.app/sendmail",
