94e648f5d85cd8d7f35fa597d7ae2178d0be205c
funwiche <funwiche.dev@gmail.com>
2026-03-08 18:52:10 +0000

    Create vNav.vue

---
+++ components/vNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative group">
+    <nuxt-link
+      :to="to"
+      class="center flex whitespace-nowrap px-3 py-1 uppercase text-xs font-bold relative"
+    >
+      {{ content }}
+      <span
+        class="absolute -bottom-2 size-2 rounded-full group-hover:bg-tint transition-colors"
+      />
+      <slot />
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel();
+defineProps<{ to: string; content: string }>();
+</script>
