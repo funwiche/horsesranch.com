2b19b7751afad2838bee4ec9ae93a8e457895e4d
funwiche <funwiche.dev@gmail.com>
2026-03-08 10:29:12 +0000

    Update vSaddleCard.vue

--- components/vSaddleCard.vue
+++ components/vSaddleCard.vue
@@ -1,14 +1,24 @@
 <template>
   <nuxt-link
     :to="`/saddles/${item.slug}`"
-    class="flex flex-col bg-white p-px text-black"
+    class="flex flex-col bg-white p-2 text-center text-black"
   >
     <img
       :src="item.images[0]"
       :alt="item.title"
       class="aspect-[3/4] object-cover object-center"
     />
-    <div class="p-3 text-lg font-bold">{{ item.title }}</div>
+    <div class="p-3">
+      <div class="text-xl font-bold mb-2 font-serif">
+        {{ item.title }}
+      </div>
+      <div class="mb-4">${{ $f.num(item.price, 0) }}</div>
+      <span
+        class="rounded bg-tint text-black opacity-90 hover:opacity-100 text-sm p-4 font-bold inline-flex"
+      >
+        Click For More Info
+      </span>
+    </div>
   </nuxt-link>
 </template>
 
