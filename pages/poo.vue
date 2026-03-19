28b899f0d02bb7859c65f14e032bdb1bef19a4eb
funwiche <funwiche.dev@gmail.com>
2026-03-19 13:13:45 +0000

    ok

--- pages/poo.vue
+++ pages/poo.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="text-[8px]">{{ saddles }}</div>
+  <pre>{{
+    horses.map((el) => {
+      let year =
+        poo.find((data) => data.slug === el.slug)?.excerpt.slice(0, 4) || "";
+      let newYear = year;
+      return {
+        ...el,
+        slug: el.slug.replaceAll(year, newYear),
+        excerpt: el.excerpt.replaceAll(year, newYear),
+        description: el.description.replaceAll(year, newYear),
+        properties: {
+          ...el.properties,
+          age: `${new Date().getFullYear() - parseInt(newYear)} years old`,
+        },
+        seoMeta: {
+          title: el.seoMeta.title.replaceAll(year, newYear),
+          desc: el.seoMeta.desc.replaceAll(year, newYear),
+          alt: el.seoMeta.alt.replaceAll(year, newYear),
+        },
+      };
+    })
+  }}</pre>
 </template>
 
 <script setup lang="ts">
 definePageMeta({ layout: "blank" });
 import saddles from "@/resources/saddles.json";
 import horses from "@/resources/horses.json";
+import poo from "@/resources/poo.json";
 </script>
 
 <style scoped></style>
