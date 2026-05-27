fc32fb21a614422b9a3b39319aea5b20178d68b2
funwiche <funwiche.dev@gmail.com>
2026-05-27 12:16:01 +0100

    Update poo.vue

--- pages/poo.vue
+++ pages/poo.vue
@@ -1,33 +1,12 @@
 <template>
-  {{ [...new Set(horses.map((el) => el.year))].sort() }} <br />
-  {{
-    [
-      ...new Set(horses.filter((el) => el.year == 2020).map((el) => el.price)),
-    ].sort()
-  }}
-  <br />
-
-  <pre>{{
-    horses.map((el) => {
-      if (el.year != 2020) return el;
-      let price = el.price < 7800 ? 7800 : el.price > 8000 ? 8000 : el.price;
-      return {
-        ...el,
-        price,
-        properties: {
-          ...el.properties,
-          price: `$${price.toFixed(2)}`,
-        },
-      };
-    })
-  }}</pre>
-  <!-- // .filter((el) => new_horses.includes(el.slug)) -->
+  <pre>{{ horses }}</pre>
 </template>
 
 <script setup lang="ts">
 definePageMeta({ layout: "blank" });
 import horses from "@/resources/horses.json";
 const new_horses = [
+  // // %2014%
   // "austin-quarter-horse-palomino-gelding",
   // "avenger-quarter-horse-red-roan-gelding",
   // "bashiru-quarter-horse-palomino-gelding",
@@ -36,9 +15,9 @@ const new_horses = [
   // "storm-quarter-horse-bay-gelding",
   // "billy-quarter-horse-palomino-gelding",
   // "parfect-quarter-horse-red-dun-gelding",
+  // // %2015%
   // "arrow-quarter-horse-sorrel-rabicano-gelding",
   // "baylee-quarter-horse-bay-roan-gelding",
-
   // "bear-quarter-horse-buckskin-gelding",
   // "beauty-quarter-horse-palomino-mare",
   // "biggy-(bugsy)-quarter-horse-buckskin-gelding",
@@ -54,23 +33,23 @@ const new_horses = [
   // "legacy-quarter-horse-blue-roan-gelding",
   // "leo-quarter-horse-chestnut-gelding",
   // "lucky-quarter-horse-buckskin-gelding",
-  "lucy-quarter-horse-buckskin-mare",
-  "mick-quarter-horse-buckskin-gelding",
-  "missy-quarter-horse-black-mare",
-  "montana-quarter-horse-palomino-gelding",
-  "nacho-quarter-horse-chestnut-gelding",
-  "olaf-quarter-horse-gray-gelding",
-  "patrick-quarter-horse-sorrel-gelding",
-  "patriot-quarter-horse-bay-roan-gelding",
-  "sahara-quarter-horse-red-roan-gelding",
-  "sandy-quarter-horse-buckskin-mare",
-  "silver-quarter-horse-buckskin-mare",
-  "stones-quarter-horse-gray-gelding",
-  "sugar-quarter-horse-buckskin-mare",
-  "sunny-quarter-horse-palomino-gelding",
-  "torch-quarter-horse-palomino-gelding",
-  "whisper-quarter-horse-palomino-gelding",
-  "wrangler-quarter-horse-red-roan-gelding",
+  // "lucy-quarter-horse-buckskin-mare",
+  // "mick-quarter-horse-buckskin-gelding",
+  // "missy-quarter-horse-black-mare",
+  // "montana-quarter-horse-palomino-gelding",
+  // "nacho-quarter-horse-chestnut-gelding",
+  // "olaf-quarter-horse-gray-gelding",
+  // "patrick-quarter-horse-sorrel-gelding",
+  // "patriot-quarter-horse-bay-roan-gelding",
+  // "sahara-quarter-horse-red-roan-gelding",
+  // "sandy-quarter-horse-buckskin-mare",
+  // "silver-quarter-horse-buckskin-mare",
+  // "stones-quarter-horse-gray-gelding",
+  // "sugar-quarter-horse-buckskin-mare",
+  // "sunny-quarter-horse-palomino-gelding",
+  // "torch-quarter-horse-palomino-gelding",
+  // "whisper-quarter-horse-palomino-gelding",
+  // "wrangler-quarter-horse-red-roan-gelding",
 ];
 </script>
 
